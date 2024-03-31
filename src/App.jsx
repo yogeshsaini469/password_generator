b89fb4d29d08a02f0047ded7f53e839055223aee
yogeshsaini469 <yogeshsaini469@gmail.com>
2024-03-31 09:49:46 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   const [password, setPassword] = useState("");
   const [copy, setCopy] = useState("Copy")
 
-  //useref Hook
   const passwordRef = useRef(null);
 
   const passwordGenerator = useCallback(() => {
